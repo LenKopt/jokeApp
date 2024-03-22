267b328c56fd1f95fbfcfcb9ec6257df81bc81e8
Alena Iadkouskaya <koptewka@mail.ru>
2024-03-22 19:17:04 +0100

    implementation User story 1

--- src/main/java/pl/akademiaspecjalistowit/jokeapp/JokeAppApplication.java
+++ src/main/java/pl/akademiaspecjalistowit/jokeapp/JokeAppApplication.java
@@ -1,17 +1,10 @@
 package pl.akademiaspecjalistowit.jokeapp;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import pl.akademiaspecjalistowit.jokeapp.controller.UserController;
-import pl.akademiaspecjalistowit.jokeapp.model.JokeDto;
 import pl.akademiaspecjalistowit.jokeapp.service.JokeService;
 import pl.akademiaspecjalistowit.jokeapp.service.JokeServiceImpl;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class JokeAppApplication {
--- src/main/java/pl/akademiaspecjalistowit/jokeapp/mapper/JokeDtoMapper.java
+++
@@ -1,8 +0,0 @@
-package pl.akademiaspecjalistowit.jokeapp.mapper;
-
-import pl.akademiaspecjalistowit.jokeapp.model.Joke;
-import pl.akademiaspecjalistowit.jokeapp.model.JokeDto;
-
-public interface JokeDtoMapper {
-    Joke toJoke(JokeDto joke);
-}
